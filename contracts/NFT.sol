9fad7ea091fef5e67f8f97b72a8574a2ab346175
Ricardo Pires <ricardo.tech@live.com>
2024-09-25 22:28:23 -0300

    feat: certificate minted event and get owned certificates

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -10,14 +10,45 @@ contract NFT is ERC1155, Ownable {
 
     uint256 public constant COURSE_CERTIFICATE = 1;
 
+    struct Certificate {
+        string studentName;
+        string courseName;
+        string completionDate;
+        string institution;
+        uint256 duration;
+    }
+
+    mapping(uint256 => Certificate) public certificates;
+
+    mapping(address => uint256[]) public ownedCertificates;
+
+    event CertificateMinted(
+        address indexed recipient,
+        uint256 tokenId, 
+        string studentName,
+        string courseName,
+        string completionDate
+    );
+
     constructor(address initialOwner)
         ERC1155("http://localhost:3000/certificates/{id}.json")
         Ownable(initialOwner)
     {}
 
-    function mintCertificate(address recipient, uint256 amount) public onlyOwner
-    {
-        _mint(recipient, COURSE_CERTIFICATE, amount, "");
+    function mintCertificate(
+        address recipient, 
+        uint256 amount,
+        string memory studentName,
+        string memory courseName,
+        string memory completionDate,
+        string memory institution,
+        uint256 duration
+    ) public onlyOwner {
+        uint256 tokenId = COURSE_CERTIFICATE;
+        _mint(recipient, tokenId, amount, "");
+        certificates[tokenId] = Certificate(studentName, courseName, completionDate, institution, duration);
+        ownedCertificates[recipient].push(tokenId);
+        emit CertificateMinted(recipient, tokenId, studentName, courseName, completionDate);
     }
 
     function uri(uint256 tokenId) public pure override returns (string memory)
@@ -29,5 +60,10 @@ contract NFT is ERC1155, Ownable {
                 ".json"
             )
         );
+    }
+
+    function getOwnedCertificates(address owner) public view returns (uint256[] memory)
+    {
+        return ownedCertificates[owner];
     }
 }
